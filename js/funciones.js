16e49c2f33d8c4bb4807cd8e072ebe0e1602b969
mikehr18 <mahr181196@gmail.com>
2019-10-09 11:32:00 -0500

    Funciones.js

---
+++ js/funciones.js
@@ -0,0 +1,20 @@
+function registrarUsuario() {
+  //$("#formregistro").submit();
+  alert(
+    $("#inputnombre").val() + $("#inputfirma").val() + $("#inputpassword").val()
+  );
+  var datos = {
+    nombre: $("#inputnombre").val(),
+    firma: $("#inputfirma").val(),
+    password: $("#inputpassword").val()
+  };
+  $.ajax({
+    url: "api.php",
+    method: "POST",
+    data: datos,
+    success: function(result) {
+      alert("Se ha registrado correctamente");
+      
+    }
+  });
+}
